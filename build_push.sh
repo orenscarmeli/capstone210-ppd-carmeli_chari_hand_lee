3204839fe5b412061cb62e1659a5a9eda4b911b4
Jon Hand <hand.jonathan.13@gmail.com>
2023-07-09 19:09:15 -0700

    writing commands for push lifecycle

--- build_push.sh
+++ build_push.sh
@@ -40,12 +40,27 @@ cd backend
 # docker pull $ACR_DOMAIN/$NAMESPACE/$IMAGE_NAME:$TAG
 # echo "container built..."
 
+###### AWS EKS Specific ######
+kubectl config use-context jon.hand@test-cluster.us-west-1.eksctl.io -n $NAMESPACE
+docker buildx build --platform linux/amxd64 -t $IMAGE_NAME:$TAG .
+
+docker tag e9ae3c220b23 aws_account_id.dkr.ecr.us-west-2.amazonaws.com/my-repository:tag
+docker tag ${IMAGE_NAME} ${IMAGE_FQDN}
+docker push aws_account_id.dkr.ecr.us-west-2.amazonaws.com/my-repository:tag
+
+
+
+kubectl create -f ./namespace.yaml
+kubectl apply -f deployment-redis.yaml -n $NAMESPACE
+kubectl apply -f service-redis.yaml -n $NAMESPACE
+kubectl apply -f deployment-pythonapi.yaml -n $NAMESPACE
+kubectl apply -f service-prediction.yaml -n $NAMESPACE
 
 # sub tag with the latest commit
 sed "s/\[TAG\]/${TAG}/g" .k8s/overlays/prod/patch-deployment-lab4_copy.yaml > .k8s/overlays/prod/patch-deployment-lab4.yaml
 
 ##### AWS ECR ######
-# docker buildx build --platform linux/amd64 -t $IMAGE_NAME:$TAG .
+# docker buildx build --platform linux/amxd64 -t $IMAGE_NAME:$TAG .
 
 # make sure we don't have other virtual services running
 # kubectl delete --all virtualservice --namespace=$NAMESPACE
@@ -57,46 +72,49 @@ kubectl apply -k .k8s/overlays/prod -n $NAMESPACE
 kubectl rollout status deployment pythonapi-deployment -n $NAMESPACE
 
 # test predict endpoint
-# curl -X POST -H 'Content-Type: application/json' localhost:8000/predict -d \
-# '''
-#     {
-#         "surveys" : [ 
-#             {
-#                "has_health_insurance": 1.0,
-#                 "difficult_doing_daytoday_tasks": 0.0,
-#                 "age_range_first_menstrual_period": 0,
-#                 "weight_change_intentional": 0,
-#                 "thoughts_you_would_be_better_off_dead": 0.0,
-#                 "little_interest_in_doing_things": 1.0,
-#                 "trouble_concentrating": 0.0,
-#                 "food_security_level_household": 2.0,
-#                 "general_health_condition": 4.0,
-#                 "monthly_poverty_index": 2.0,
-#                 "food_security_level_adult": 2.0,
-#                 "count_days_seen_doctor_12mo": 4.0,
-#                 "has_overweight_diagnosis": 1.0,
-#                 "feeling_down_depressed_hopeless": 0.0,
-#                 "count_minutes_moderate_recreational_activity": 15.0,
-#                 "have_liver_condition": 0,
-#                 "pain_relief_from_cardio_recoverytime": 1.0,
-#                 "education_level": 5.0,
-#                 "count_hours_worked_last_week": 40.0,
-#                 "age_in_years": 44.0,
-#                 "has_diabetes": 1.0,
-#                 "alcoholic_drinks_past_12mo": 5.0,
-#                 "count_lost_10plus_pounds": 3.0,
-#                 "days_nicotine_substitute_used": 0,
-#                 "age_with_angina_pectoris": 33.0,
-#                 "annual_healthcare_visit_count": 3.0,
-#                 "poor_appetitie_or_overeating": 1.0,
-#                 "feeling_bad_about_yourself": 0.0,
-#                 "has_tried_to_lose_weight_12mo": 0.0,
-#                 "count_days_moderate_recreational_activity": 2.0,
-#                 "count_minutes_moderate_sedentary_activity": 960.0
-#             }
-#         ]
-#     }
-# '''
+curl -X POST -H 'Content-Type: application/json' 'http://54.193.212.89:8000/predict' -d \
+'''
+    {
+        "surveys" : [ 
+            {
+               "has_health_insurance": 1.0,
+                "difficult_doing_daytoday_tasks": 0.0,
+                "age_range_first_menstrual_period": 0,
+                "weight_change_intentional": 0,
+                "thoughts_you_would_be_better_off_dead": 0.0,
+                "little_interest_in_doing_things": 1.0,
+                "trouble_concentrating": 0.0,
+                "food_security_level_household": 2.0,
+                "general_health_condition": 4.0,
+                "monthly_poverty_index": 2.0,
+                "food_security_level_adult": 2.0,
+                "count_days_seen_doctor_12mo": 4.0,
+                "has_overweight_diagnosis": 1.0,
+                "feeling_down_depressed_hopeless": 0.0,
+                "count_minutes_moderate_recreational_activity": 15.0,
+                "have_liver_condition": 0,
+                "pain_relief_from_cardio_recoverytime": 1.0,
+                "education_level": 5.0,
+                "count_hours_worked_last_week": 40.0,
+                "age_in_years": 44.0,
+                "has_diabetes": 1.0,
+                "alcoholic_drinks_past_12mo": 5.0,
+                "count_lost_10plus_pounds": 3.0,
+                "days_nicotine_substitute_used": 0,
+                "age_with_angina_pectoris": 33.0,
+                "annual_healthcare_visit_count": 3.0,
+                "poor_appetitie_or_overeating": 1.0,
+                "feeling_bad_about_yourself": 0.0,
+                "has_tried_to_lose_weight_12mo": 0.0,
+                "count_days_moderate_recreational_activity": 2.0,
+                "count_minutes_moderate_sedentary_activity": 960.0
+            }
+        ]
+    }
+'''
+
+curl -o /dev/null -s -w "%{http_code}\n" -X GET 'http://network-load-balancer-867733373408bca0.elb.us-west-1.amazonaws.com/health'
+
 
 # kubectl delete --all service --namespace=$NAMESPACE
 # kubectl delete --all deployment --namespace=$NAMESPACE
