3ce48e762926a7e859e906ace8a3bcd6764cafb6
Jon Hand <hand.jonathan.13@gmail.com>
2023-07-10 08:54:30 -0700

    streamlit reconect and kill session cmds

--- streamlit_run.sh
+++ streamlit_run.sh
@@ -1,6 +1,8 @@
-cd
+cd w210-streamlit
 tmux new -s StreamSession
 streamlit run 210_Home.py
 # ctrl + b
 # d
 # tmux attach -t StreamSession
+# ps aux | grep streamlit | sed /ubuntu/
+$ ps aux | grep streamlit | grep -Po 'ubuntu\s+\d+' | grep -Po '\d+'
