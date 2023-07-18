5d1a551a9ddef7d25e6acd858f24e82ebc20a71f
Jon Hand <hand.jonathan.13@gmail.com>
2023-07-17 18:21:15 -0700

    using answer dict

--- streamlit/pages/6_Post_Test.py
+++ streamlit/pages/6_Post_Test.py
@@ -14,39 +14,7 @@ def PostRequestSurveyAPI(answer_dict):
     url = 'http://network-load-balancer-3ec3c60f32bd38c8.elb.us-west-1.amazonaws.com/predict'
     myobj = {
         "surveys" : [ 
-            {
-            "has_health_insurance": 1.0,
-                "difficult_doing_daytoday_tasks": 0.0,
-                "age_range_first_menstrual_period": 0,
-                "weight_change_intentional": 0,
-                "thoughts_you_would_be_better_off_dead": 0.0,
-                "little_interest_in_doing_things": 1.0,
-                "trouble_concentrating": 0.0,
-                "food_security_level_household": 2.0,
-                "general_health_condition": 4.0,
-                "monthly_poverty_index": 2.0,
-                "food_security_level_adult": 2.0,
-                "count_days_seen_doctor_12mo": 4.0,
-                "has_overweight_diagnosis": 1.0,
-                "feeling_down_depressed_hopeless": 0.0,
-                "count_minutes_moderate_recreational_activity": 15.0,
-                "have_liver_condition": 0,
-                "pain_relief_from_cardio_recoverytime": 1.0,
-                "education_level": 5.0,
-                "count_hours_worked_last_week": 40.0,
-                "age_in_years": 44.0,
-                "has_diabetes": 1.0,
-                "alcoholic_drinks_past_12mo": 5.0,
-                "count_lost_10plus_pounds": 3.0,
-                "days_nicotine_substitute_used": 0,
-                "age_with_angina_pectoris": 33.0,
-                "annual_healthcare_visit_count": 3.0,
-                "poor_appetitie_or_overeating": 1.0,
-                "feeling_bad_about_yourself": 0.0,
-                "has_tried_to_lose_weight_12mo": 0.0,
-                "count_days_moderate_recreational_activity": 2.0,
-                "count_minutes_moderate_sedentary_activity": 960.0
-            }
+           answer_dict
         ]
     }
     x = requests.post(url, json=myobj)
