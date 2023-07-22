f7d01ac0253de0d6a0840135eb75092ae8fe7bc5
Jon Hand <hand.jonathan.13@gmail.com>
2023-07-22 13:45:59 -0700

    testing streamlit file upload to server

--- streamlit/pages/4_Imaging_Model.py
+++ streamlit/pages/4_Imaging_Model.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import gzip
+import nibabel as nb
+import os
+
+dirname = os.path.dirname(os.path.realpath('__file__'))
+
 
 st.set_page_config(page_title="Neuroimaging Model", page_icon="")
 
@@ -12,4 +18,10 @@ st.write(
     """
 )
 
-st.file_uploader('Upload fMRI file (specify format):')
+uploaded_file = st.file_uploader('Upload fMRI file (compressed .gz or /nii):', type=['gz', 'nii'])
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name, "FileType":uploaded_file.type}
+    st.write(file_details)
+    with open(os.path.join('./imageUploads', uploaded_file.name), "wb") as f: 
+        f.write(uploaded_file.getbuffer())         
+    st.success("Uploaded File")
