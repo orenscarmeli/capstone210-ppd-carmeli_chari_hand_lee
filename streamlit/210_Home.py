b66ec0352a96d8ebcec38248311d4f0314193e33
julielee48 <63068846+julielee48@users.noreply.github.com>
2023-08-05 11:55:01 -0700

    updated reference section

--- streamlit/210_Home.py
+++ streamlit/210_Home.py
@@ -5,18 +5,17 @@ hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
        footer {visibility: hidden;}
-       
        </style>
        """
 st.set_page_config(
     page_title="Welcome to Velvaere",
-    page_icon="./page_logo.png",
-    # page_bg_color="0e0e35",
+    page_icon="./page_logo.png"
 )
-
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
+
 st.sidebar.header("Home")
+
 st.image("./banner2.png")
 st.write("")
 st.write("")
@@ -25,14 +24,10 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("**What is Bridges?**")
-    st.write(
-        "Bridges is a web app by Velvaere offering two machine learning based prediction tools to support PPD (Postpartum Depression) diagnosis."
-    )
+    st.write("Bridges is a web app by Velvaere offering two machine learning based prediction tools to support PPD (Postpartum Depression) diagnosis.")
 with col2:
     st.subheader("**Why do we need it?**")
-    st.write(
-        "PPD is observed in 10-15% of new mothers but many cases aren't recognized. PPD diagnosis is important to ensure mother and baby safety and wellbeing as difficult temperament, poor self-regulation, and behavior problems have been observed in infants of depressed mothers. Bridges aims to improve diagnosis by using multimodal ML based approaches to prediction."
-    )
+    st.write("PPD is observed in 10-15% of new mothers but many cases aren't recognized. PPD diagnosis is important to ensure mother and baby safety and wellbeing as difficult temperament, poor self-regulation, and behavior problems have been observed in infants of depressed mothers. Bridges aims to improve diagnosis by using multimodal ML based approaches to prediction.")
 
 st.divider()
 
@@ -53,16 +48,15 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("**Tell me more about PPD**")
-    st.write(
-        "The diagnosis criteria by the American Psychiatric Association is 2+ weeks of persistent:"
-    )
+    st.write("The diagnosis criteria by the American Psychiatric Association is 2+ weeks of persistent:")
     st.write("1. Depressed mood or")
-    st.write(
-        "2. Loss of interest in daily activities with four associated symptoms onsetting within 4 weeks after birth"
-    )
-    st.write(
-        "Currently the Edinburgh Postnatal Depression Scale is used for screening but there is no diagnostic test for PPD."
-    )
+    st.write("2. Loss of interest in daily activities with four associated symptoms onsetting within 4 weeks after birth")
+    st.write("Currently the Edinburgh Postnatal Depression Scale is used for screening but there is no diagnostic test for PPD.")
 with col2:
     st.subheader("**What relevant research is out there?**")
-    st.write("Link to papers with executive summary")
+    st.markdown("[Wilkie, E.,](https://www.cambridge.org/core/journals/european-psychiatry/article/prediction-of-postpartum-depression-and-anxiety-based-on-clinical-interviews-and-symptom-selfreports-of-depression-and-anxiety-during-pregnancy/C86C9329EAF29521377E5BF9327A9650/) Gillet, V., Talati, A., Posner, J., & Takser, L. (2022). Prediction of post-partum depression and anxiety based on clinical interviews and symptom self-reports of depression and anxiety during pregnancy. European Psychiatry, 65(S1), S268–S269.")
+    st.markdown("[Bloch, M.,](https://pubmed.ncbi.nlm.nih.gov/16377359/) Rotenberg, N., Koren, D., & Klein, E. (2006b). Risk factors for early postpartum depressive symptoms. General Hospital Psychiatry, 28(1), 3–8.")
+    st.markdown("[Gao, S.,](https://onlinelibrary.wiley.com/doi/10.1111/cns.13048) Calhoun, V., & Sui, J. (2018). Machine learning in major depression: From classification to treatment outcome prediction. CNS Neuroscience & Therapeutics, 24(11), 1037–1052.")
+
+
+
